1da1a775690111f872d2969ddfd87b1f2d2fca5b
SIeepii <pyao477@gmail.com>
2023-07-14 18:33:48 -0400

    fixed spacing between backgrounds

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 function drawBackground(ref, ctx, keyframes, speed, frame){
     if(ref){
-      ctx.drawImage(ref, 0, 600*frame, 900, 600, -(speed*keyframes%900) + 900, 0, 900, 600)
+      ctx.drawImage(ref, 0, 600*frame, 900, 600, -(speed*keyframes%900) + 899, 0, 900, 600)
       ctx.drawImage(ref, 0, 600*frame, 900, 600, -(speed*keyframes%900), 0, 900, 600)
     }
 }
@@ -15,8 +15,13 @@ function getBackground(ref, context, keyframes){
     drawBackground(ref, context, keyframes, 1.2, 4, 0)//front water
 }
 
+const canvas = document.querySelector("canvas")
 const img = new Image()
 let keyframes = 0;
+canvas.width = 900;
+canvas.height = 600;
+
+const ctx = canvas.getContext("2d")
 
 function init(){
     img.src = "background.png"
@@ -24,17 +29,8 @@ function init(){
 }
 
 function draw(){
-    const canvas = document.querySelector("canvas")
-    canvas.width = 900;
-    canvas.height = 600;
-    const ctx = canvas.getContext("2d")
-
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
     getBackground(img, ctx, keyframes)
     keyframes++
-    console.log(keyframes)
-
     window.requestAnimationFrame(draw)
 }
 
