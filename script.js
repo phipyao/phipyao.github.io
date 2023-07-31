4089b5cf4dee418216ae2fa61a4317358759661d
SIeepii <pyao477@gmail.com>
2023-07-31 13:29:56 -0400

    pushing p

--- script.js
+++ script.js
@@ -2,16 +2,17 @@ const canvas = document.querySelector("canvas")
 const img = new Image()
 let keyframes = 0;
 canvas.width = window.innerWidth;
-canvas.height = 600;
+canvas.height = window.innerHeight
 
 images = Math.floor(canvas.width/900) + 2
 
 const ctx = canvas.getContext("2d")
 
+
 function drawBackground(ref, ctx, keyframes, speed, frame){   
     if(ref){
         for(let i = 0; i < images; i++){
-            ctx.drawImage(ref, 0, 600*frame, 900, 600, -(speed*keyframes%900) + (i*899), 0, 900, 600)
+            ctx.drawImage(ref, 0, 600*frame, 900, 600, -(speed*keyframes%900) + (i*899), 17, 900, 617)
         }        
     }
 }
@@ -27,12 +28,19 @@ function getBackground(ref, context, keyframes){
 //Handles Dynamic Background Resizes
 window.addEventListener('resize', resizeCanvas, false);
 function resizeCanvas() {
-    canvas.width = window.innerWidth;
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight
+    ctx.fillStyle = 'rgb(113, 138, 199)'
+    ctx.fillRect(0, 617, canvas.width, canvas.height)
     images = Math.floor(canvas.width/900) + 2
+
+    
 }
 
 function init(){
     img.src = "background.png"
+    ctx.fillStyle = 'rgb(113, 138, 199)'
+    ctx.fillRect(0, 617, canvas.width, canvas.height-617)
     window.requestAnimationFrame(draw)
 }
 
